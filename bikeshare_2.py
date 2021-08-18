4387d43afb3ff4ff5ef28ecb7b9dae18d2e9c1d7
Meshal Alsaleem <sa.meshal.sa@gmail.com>
2021-08-18 18:02:16 +0300

    type: refactor
    The condition for while clause to check the length of a list was comparing if the length was either larger or smaller than 3. In other words, whether the length is NOT 3.
    
    For readability purposes it has been changed to view the code in a simpler picture.
    
    Check line #31

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -18,17 +18,17 @@ def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     months = ['january', 'february', 'march', 'april', 'may', 'june', 'all']
-    days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'all']  
+    days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'all']
     cities = ['chicago', 'new york city', 'washington']
-    
+
 
 
     inputCityMonthDay = input("Please enter the input according to this format => CITY, MONTH, DAY\nFor example: Chicago, June, All\n").lower()
-    
+
     # Split string and assign city, month, day accordingly
     inputList = inputCityMonthDay.strip().split(',')
-    
-    while len(inputList) < 3 or len(inputList) > 3:
+
+    while len(inputList) != 3:
         inputCityMonthDay = input("Input is entered incorrectly (either extra variables, or missing variables, have been entered)\nPlease enter the input according to this format => CITY, MONTH, DAY\nFor example: Chicago, June, All\n").lower()
         inputList = inputCityMonthDay.strip().split(',')
 
@@ -43,19 +43,19 @@ def get_filters():
         city = input("Please enter the city you wish to view its data: (Options: Chicago, Washington, New York City) ").lower()
 
 
-       
+
     while month not in months:
         print("Can not filter by specified value")
-        month = input("Would you like to filter by month or by day? (Answer by typing 'day' or 'month', if no filter is desired enter 'all')").lower()    
+        month = input("Would you like to filter by month or by day? (Answer by typing 'day' or 'month', if no filter is desired enter 'all')").lower()
 
 
-    
+
     while day not in days:
         print("The day entered is not availible")
         day = input("Please enter the day where you wish to view the data: (Options: Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)").lower()
-       
-   
-    
+
+
+
 
     print('-'*40)
     return city, month, day
@@ -96,7 +96,7 @@ def load_data(city, month, day):
 
         # Create a new filtered dataframe
         df = df[df['day_of_week'] == day.title()]
-        
+
 
 
     return df
@@ -106,7 +106,7 @@ def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
-    
+
     start_time = time.time()
 
     # display the most common month
@@ -136,7 +136,7 @@ def station_stats(df):
 
     # display most commonly used start station
     popular_start_station = df['Start Station'].mode()[0]
-   
+
 
     print("Most popular start station {}".format(popular_start_station))
     # display most commonly used end station
@@ -147,9 +147,9 @@ def station_stats(df):
     # create a column displaying the full trip then proceed to find the mode of the newly created column
     df['Start End Station'] = df['Start Station'].map(str) + ' - '+ df['End Station'].map(str)
     popular_combination = df['Start End Station'].mode()[0]
-    
+
     print("Most popular combination is {}".format(popular_combination))
-   
+
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -206,16 +206,16 @@ def view_users(df):
     """Displays individual informaion of bikeshare users."""
 
     print_sample = input("Would you like to view 5 rows of data for bikeshare users? (Enter 'yes' or 'no')").lower()
-    
 
-    # Check input validity 
+
+    # Check input validity
     while print_sample not in ['yes', 'no']:
         print("The response is invalid, please enter either 'Yes' or 'No'")
         print_sample = input("Would you like to view 5 rows of data for bikeshare users? (Enter 'yes' or 'no')").lower()
     start_loc = 0
     # If user enters 'yes' proceed to create a dictionary from the dataframe and print index by index the rows into the console
     while print_sample != 'no':
-        
+
         print(df.iloc[start_loc:start_loc + 5])
         start_loc += 5
         print_sample = input("Do you wish to continue?: ").lower()
@@ -235,7 +235,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
         view_users(df)
-       
+
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
